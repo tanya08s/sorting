bf9b4ed5e2c1a2da2e8e4c1290ee1c3760e88e8a
tanya08s <tanyasingh8750@gmail.com>
2021-02-11 19:13:44 +0530

    new sort

---
+++ src/sorting/newsort.java
@@ -0,0 +1,15 @@
+package sorting;
+import java.util.Arrays;
+
+public class newsort {
+
+	public static void main(String[] args) {
+		int data[] = {3, 6, -2, 5, 1};
+			Arrays.sort(data);
+			for (int c: data) {
+				System.out.println(c);
+			}
+
+	}
+
+}
